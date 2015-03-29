74134b3622fc773c10bcc7394e90ff7816b655f2
siman-man <k128585@ie.u-ryukyu.ac.jp>
2015-03-29 23:33:28 +0900

    Add chebyshev distance

--- lib/algorithmy/grid/grid.rb
+++ lib/algorithmy/grid/grid.rb
@@ -2,4 +2,8 @@ module Kernel
   def manhattan(y1, x1, y2, x2)
     (y2 - y1).abs + (x2 - x1).abs
   end
+
+  def chebyshev(y1, x1, y2, x2)
+    [(y2 - y1).abs, (x2 - x1).abs].max
+  end
 end
--- spec/grid/grid_spec.rb
+++ spec/grid/grid_spec.rb
@@ -1,7 +1,15 @@
 require 'spec_helper'
 
 describe Algorithmy do
-  it 'Grid function' do
-    expect(manhattan(0, 0, 3, 3)).to eq 6
+  describe 'Grid function' do
+    it 'Manhattan distance' do
+      expect(manhattan(0, 0, 3, 3)).to eq 6
+      expect(manhattan(-2, -2, 3, 3)).to eq 10
+    end
+
+    it 'Chebyshev distance' do
+      expect(chebyshev(0, 0, 3, 3)).to eq 3
+      expect(chebyshev(0, 0, 1, 3)).to eq 3
+    end
   end
 end
